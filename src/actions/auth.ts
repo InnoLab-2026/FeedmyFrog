3b3d25a1e3e56e9070380ba70312544992455a46
mklemmingen <LauterbachMK@protonmail.com>
2026-05-22 11:30:00 +0200

    add logout server action (foundation)

---
+++ src/actions/auth.ts
@@ -0,0 +1,8 @@
+'use server';
+import { redirect } from 'next/navigation';
+import { destroySession } from '@/lib/session';
+
+export async function logout() {
+  await destroySession();
+  redirect('/login');
+}
